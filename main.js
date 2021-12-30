9ec1ccff73a96c887d5f52867ac57262ff78da2a
ChrisTom-94 <christian.tomasino.dev@gmail.com>
2021-12-30 18:30:11 +0100

    [TO DO] add web worker

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 import './style.css';
 import * as d3 from "d3";
 import * as topojson from "topojson-client";
-import { difference } from 'd3';
+
+const worker = new Worker("./worker.js");
 
 const width = 500;
 const height = 500;
-const speedRotation = 1;
+let speedRotation = 3;
+d3.select('#speed-rotation').on("change", e => speedRotation = parseInt(e.target.value));
 
-let timestamp = 0, animationRotation;
+let timestamp = 0;
 
 const projection = d3.geoOrthographic().translate([width / 2, height / 2]).precision(.1);
 const canvas = d3.select("#canvas-container").append("canvas");
@@ -18,6 +20,8 @@ let paths, countries;
 
 (async () => {
     const data = await d3.json('https://unpkg.com/world-atlas@1/world/110m.json');
+    // const csv = await d3.csv("./data/gpw_v4_admin_unit_center_points_population_estimates_rev11_global.csv")
+    // console.log(csv);
     paths = topojson.feature(data, data.objects.land)
     countries = topojson.feature(data, data.objects.countries)
     requestAnimationFrame(render)
@@ -40,8 +44,8 @@ function strokePath(path, color){
 }
 
 function draw(){
-    fillPath({type: "Sphere"}, "#0000ff");
-    fillPath(paths, "#ff00ff");
+    fillPath({type: "Sphere"}, "#1a285a");
+    fillPath(paths, "#3b5842");
 }
 
 function rotate(difference){
